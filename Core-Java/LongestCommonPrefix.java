15257535de3c10c7dfd06542f17b09dbbf6ff63a
Anuj <sajwananuj2@gmail.com>
2026-01-18 22:24:25 +0530

    added two array problems

---
+++ Core-Java/LongestCommonPrefix.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class LongestCommonPrefix {
+
+    public static String longCommonPrefix(String[] strs){
+        Arrays.sort(strs);
+        String s1 = strs[0];
+        String s2 = strs[strs.length-1];
+
+        int i=0;
+
+        while(i<s1.length() && i<s2.length()){
+            if(s1.charAt(i) == s2.charAt(i)){
+                i++;
+            }else{break;}
+        }
+        return s1.substring(0,i);
+    }
+
+    public static void main(String[] args) {
+        String[] s = {"flower","flow","flight"};
+//        System.out.print("LongestCommonPrefix : ");
+        System.out.println("LongestCommonPrefix : " + longCommonPrefix(s));
+    }
+}
---
+++ Core-Java/RemoveDuplicates.java
@@ -0,0 +1,17 @@
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println("After Duplicates are removed : " + removeDuplicates(nums));
+    }
+//number of unique elements are
+    public static int removeDuplicates(int[] nums){
+        int i=1;
+        for(int j=1; j<nums.length; j++){
+            if(nums[j] != nums[j-1]){
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
